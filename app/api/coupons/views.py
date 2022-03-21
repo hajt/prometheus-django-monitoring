e087a4ff7ee0663517d73558c696bbdd7f50dd0c
Rafał Kondziela <hajtpl@gmail.com>
2022-03-21 18:25:59 +0100

    Add prometheus metrics

--- app/api/coupons/views.py
+++ app/api/coupons/views.py
@@ -1,9 +1,16 @@
+from django.urls import reverse
 from django.utils.timezone import now, timedelta
 
 from rest_framework import viewsets
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
+from .metrics import (
+    coupon_create_last_status,
+    coupon_create_time,
+    last_user_activity_time,
+    requests_total,
+)
 from .models import Coupon, UserCoupon
 from .serializers import UserCouponDetailSerializer, UserCouponSerializer
 
@@ -19,10 +26,19 @@ class UserCouponViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         return UserCouponSerializer if self.action == "list" else UserCouponDetailSerializer
 
+    def initial(self, request, pk=None, *args, **kwargs):
+        user = self.request.user
+        requests_total.labels(
+            endpoint=self._get_endpoint(pk), method=self.request.method, user=user
+        ).inc()
+        last_user_activity_time.labels(user=user).set(now().timestamp())
+        return super().initial(request, *args, **kwargs)
+
     def create(self, request):
         user = self.request.user
 
         if user.coupons.filter(valid=True):
+            coupon_create_last_status.labels(user=user).state("error")
             raise ValidationError({"error": "Valid coupon already exists"})
 
         lock_in_minutes = 5
@@ -35,6 +51,7 @@ class UserCouponViewSet(viewsets.ModelViewSet):
         )
 
         if last_user_coupon:
+            coupon_create_last_status.labels(user=user).state("error")
             time_left = (last_user_coupon.created_at + timedelta(minutes=lock_in_minutes)) - now()
             raise ValidationError(
                 {
@@ -46,7 +63,15 @@ class UserCouponViewSet(viewsets.ModelViewSet):
         coupon = Coupon.objects.order_by("?").first()
 
         if not coupon:
+            coupon_create_last_status.labels(user=user).state("error")
             raise ValidationError({"error": "No coupons defined"})
 
+        start_time = now().timestamp()
         user_coupon = UserCoupon.objects.create(coupon=coupon, user=user)
+        coupon_create_time.observe(now().timestamp() - start_time)
+        coupon_create_last_status.labels(user=user).state("success")
+
         return Response(UserCouponDetailSerializer(user_coupon).data)
+
+    def _get_endpoint(self, pk=None):
+        return f"{reverse('coupons-list')}{pk}/" if pk else reverse("coupons-list")
